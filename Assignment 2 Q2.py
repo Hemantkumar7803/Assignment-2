1f1e8b92cae029bffb69569f5a910b96fdf1d46a
Hemantkumar7803 <121182169+Hemantkumar7803@users.noreply.github.com>
2022-12-22 19:38:58 +0530

    Create Assignment 2 Q2.py

---
+++ Assignment 2 Q2.py
@@ -0,0 +1,6 @@
+# Assignment-2
+name="HemantKumar"
+sid=22105106
+dept="Electronics and communication engineering"
+cg=10.0
+print(" Hey,",name,"Here!\n","My SID is",sid,"\n","I am from",dept,"department and my CGPA is",cg)
